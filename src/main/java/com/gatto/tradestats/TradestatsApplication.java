481bdff76b09651902dcef71211176e92e8ae631
Andrei Koshelap <andreikoshelap@gmail.com>
2026-07-23 10:34:15 +0300

    intitial commit

---
+++ src/main/java/com/gatto/tradestats/TradestatsApplication.java
@@ -0,0 +1,13 @@
+package com.gatto.tradestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatto/tradestats/TradestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatto.tradestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
